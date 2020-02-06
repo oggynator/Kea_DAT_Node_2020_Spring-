91779a6a1a15551da10b4fcaeecb45f02bb37382
oggynator <august.hejberg@gmail.com>
2020-02-06 09:47:38 +0100

    First exercises

---
+++ 1/1.intro/1.intro/Intro_exercises/1.Basics.js
@@ -0,0 +1,12 @@
+//Exercise 1
+var firstName = "Anders";
+var lastName = "Latif ";
+console.log("My firstname is ",firstName, " and my last name is ",lastName);
+
+//Exercise 2 
+
+var year = "2017";
+var number =1;
+
+var newNumber = parseInt(year) + number;
+console.log(newNumber)
